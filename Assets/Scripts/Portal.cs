ce0c42aab9562700b3f3fa9e137aba58ad0ae14e
PoorviPrakash <poorvibprakash@gmail.com>
2022-04-10 03:46:27 -0700

    Bug Fixes, Portal fixes and Coin animation

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,13 +7,13 @@ public class Portal : MonoBehaviour
     //public static Vector2 Checkpoint;
     public static bool portalHit = false;
 
-    [SerializeField] private float speed = 1f;
+    [SerializeField] private float speed;
 
     [SerializeField] private AudioSource portalSound;
 
     void Update()
     {
-        transform.Rotate(180f * speed * Time.deltaTime,0f ,0f);
+        transform.Rotate(90 * speed * Time.deltaTime, 0f, 0f);
     }
 
 
