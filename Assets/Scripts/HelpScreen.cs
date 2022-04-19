93e0a98afd7123eb878babbeaf1a6cb674f457ad
PoorviPrakash <poorvibprakash@gmail.com>
2022-04-18 18:47:19 -0700

    Help menu icon and screen

---
+++ Assets/Scripts/HelpScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HelpScreen : MonoBehaviour
+{
+    public static void LoadScreen()
+    {
+        Debug.Log("In help");
+        Time.timeScale = 0.0f;
+        SceneManager.LoadScene("HelpScreen");
+    }
+}
